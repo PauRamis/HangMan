ab3177d2bc6f5ec862653a636290d325c8f36a7d
Your Name <you@example.com>
2022-03-04 18:56:12 +0100

    imput

--- src/GameController.java
+++ src/GameController.java
@@ -1,9 +1,11 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class GameController {
 private AlphabetDisplay alphabetDisplay;
 private WordDisplay wordDisplay;
 private Gallows gallows;
+private Scanner scanner = new Scanner(System.in);
 
     GameController(AlphabetDisplay ad, WordDisplay wd, Gallows g) {
         this.alphabetDisplay = ad;
@@ -13,16 +15,30 @@ private Gallows gallows;
 
     void playGame(Word w) {
         Alphabet alphabet = new Alphabet();
-        //Mentre no hagi acabat, mostram el
         while (!gallows.isHung() && !w.isGuessed()){
-            //Mostrem estat del joc
+            //Mostrem l'estat del joc
             this.gallows.display();
             this.wordDisplay.display(w);
             this.alphabetDisplay.display(alphabet);
+
+            char c = getInput();
+
+
         }
     }
 
     private char getInput() {
-        return 0;
+        while (true){
+            String line = scanner.nextLine().toLowerCase(Locale.ROOT);
+            //Validam l'entrada de l'usuari
+            if (line.length() == 1) {
+                char c = line.charAt(0);
+                if (c >= 'a' && c<= 'z')
+                return line.charAt(0);
+            } else {
+                System.out.println("Entrada no valida");
+            }
+        }
+
     }
 }
