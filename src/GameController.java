cc6ee56ec3e77f0edf0c0821eddd5cd3f79a5581
Your Name <you@example.com>
2022-03-04 18:37:50 +0100

    good copy

--- src/GameController.java
+++ src/GameController.java
@@ -1,14 +1,20 @@
 import java.util.Scanner;
 
 public class GameController {
-
+private AlphabetDisplay alphabetDisplay;
+private WordDisplay wordDisplay;
+private Gallows gallows;
 
     GameController(AlphabetDisplay ad, WordDisplay wd, Gallows g) {
-
+        this.alphabetDisplay = ad;
+        this.wordDisplay = wd;
+        this.gallows = g;
     }
 
     void playGame(Word w) {
+        while (!gallows.isHung() && w.isGuessed()){
 
+        }
     }
 
     private char getInput() {
--- src/HangMan.java
+++ src/HangMan.java
@@ -4,16 +4,28 @@ import java.util.Scanner;
  * Created by pnegre on 04/04/16.
  */
 public class HangMan {
+    private WordFactory wordFactory;
 
     HangMan(WordFactory wf) {
+        this.wordFactory = wf;
     }
 
     public void play() {
-    }
+        AlphabetDisplay alphabetDisplay =  new TextAlphabetDisplay();
+        WordDisplay wordDisplay = new TextWordDisplay();
+        Gallows gallows = new TextGallows();
+        GameController gameController = new GameController(
+                alphabetDisplay,
+                wordDisplay,
+                gallows
+        );
+        gameController.playGame(this.wordFactory.makeWord());
+     }
 
     public static void main(String[] args) {
         WordFactory wordFactory = new DefaultWordFactory();
         HangMan hangMan = new HangMan(wordFactory);
+        hangMan.play();
     }
 }
 
